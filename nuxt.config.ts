d7d376d6f07fab78ea2f043eb290515da0d4dbbc
eralinkd <eralinkd@gmail.com>
2025-09-04 12:28:15 +0300

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    '@vueuse/motion/nuxt',
+  ],
+
+  css: ['~/assets/css/tailwind.css'],
+})
