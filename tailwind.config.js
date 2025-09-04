6bf708d89e4b05520a2ff7e464de34730f0c7cce
eralinkd <eralinkd@gmail.com>
2025-09-04 12:36:15 +0300

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-import type { Config } from 'tailwindcss'
-
 export default {
   content: [
     './app/**/*.{vue,js,ts}',
@@ -34,4 +32,4 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config
+}
