6316a578807d09e0fef4451b2be95f8e38c60b4d
eralinkd <eralinkd@gmail.com>
2025-09-14 18:11:59 +0300

    mobile version

--- app/components/Why/index.vue
+++ app/components/Why/index.vue
@@ -1,25 +1,31 @@
 <template>
   <div id="why" class="w-full flex flex-col items-center">
     <h2
-      class="font-gilroy font-semibold text-4xl leading-none tracking-normal text-text self-start"
+      class="font-gilroy font-semibold text-3xl leading-none tracking-normal text-text self-start md:hidden"
+    >
+      Почему <span class="text-accent">стоит доверить</span> организацию
+      <span class="text-accent">Backstage</span>
+    </h2>
+    <h2
+      class="font-gilroy font-semibold text-4xl leading-none tracking-normal text-text self-start hidden md:block"
     >
       Почему <span class="text-accent">стоит доверить</span> <br />
       организацию <span class="text-accent">Backstage</span>
     </h2>
     <p class="body-text mx-auto mt-3">
-      Мы создаем не просто фотосессии, а уникальные истории, <br />
+      Мы создаем не просто фотосессии, а уникальные истории, <br class="hidden lg:block" />
       где каждая деталь отражает вашу индивидуальность
     </p>
     <div class="mt-7">
-      <NuxtImg
-        src="/images/why/image.png"
+      <img
+        src="./image.png"
         alt="Почему стоит выбрать Backstage"
         class="w-full h-auto object-cover hidden md:block"
-        format="webp,jpg"
-        quality="85"
-        loading="eager"
-        preload
-        draggable="false"
+      />
+      <img
+        src="./why-mobile.png"
+        alt="Почему стоит выбрать Backstage"
+        class="w-full h-auto object-cover block md:hidden"
       />
     </div>
   </div>
