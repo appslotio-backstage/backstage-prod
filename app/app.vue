6e3701b5cc60b7967e7956f1b6fc3effe397c24c
eralinkd <eralinkd@gmail.com>
2025-09-15 21:14:36 +0300

    globals

--- app/app.vue
+++ app/app.vue
@@ -8,12 +8,12 @@
 const globals = await useGlobals()
 
 useHead({
-  title: globals.value.meta.defaultMeta.siteName,
+  title: globals.value.meta.title,
   meta: [
-    { name: 'description', content: globals.value.meta.defaultMeta.description },
-    { property: 'og:title', content: globals.value.meta.defaultMeta.title },
-    { property: 'og:description', content: globals.value.meta.defaultMeta.description },
-    { property: 'og:image', content: globals.value.meta.defaultMeta.ogImage },
+    { name: 'description', content: globals.value.meta.seo.description },
+    { property: 'og:title', content: globals.value.meta.seo.ogTitle },
+    { property: 'og:description', content: globals.value.meta.seo.ogDescription },
+    { property: 'og:image', content: globals.value.meta.seo.ogImage },
   ],
   link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
 })
